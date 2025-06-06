08172bf318c83077b2975999e69205ca1d2fb1b4
xhide341 <shawnehgn10@gmail.com>
2025-06-06 15:15:01 +0800

    feat: add header and reset content

---
+++ src/components/header.tsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+  return (
+    <header className="w-full px-4 h-16 flex items-center justify-between fixed top-0 z-50 border-b-2 border-red-500 border-dotted">      
+      <h1>Header</h1>
+    </header>
+  );
+};
+
+export default Header;

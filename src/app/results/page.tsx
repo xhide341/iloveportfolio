1db874a4725a71f237c43632ca1894f5785d044d
Yncy0 <paalayancy@gmail.com>
2025-06-08 09:51:21 +0800

    added: simple layout

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -1,5 +1,24 @@
 export default function ResultsPage() {
+  type Foo = {
+    instructions: string
+  }
+
+  const foo: Foo[] = [
+    { instructions: "Instructions 1" },
+    { instructions: "Instructions 2" },
+    { instructions: "Instructions 3" }
+  ]
+
   return (
-    <div>Results</div>
+    <main>
+      <h1>Your portfolio website is ready</h1>
+      <section>
+        <ul className="flex flex-row justify-center align-center gap-10">
+          {foo.map((item, index) => (
+            <li key={index}>{item.instructions}</li>
+          ))}
+        </ul>
+      </section>
+    </main>
   )
 }

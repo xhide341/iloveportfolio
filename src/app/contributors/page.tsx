cef6d6a9a14b4ce6560f8e96a90020b9463868e0
Yncy0 <paalayancy@gmail.com>
2025-06-23 22:36:56 +0800

    finished: added responsive layout

--- src/app/contributors/page.tsx
+++ src/app/contributors/page.tsx
@@ -34,18 +34,18 @@ export default function ContributorsPage() {
   ];
 
   return (
-    <main className="flex h-full w-[1200px] flex-col items-start text-latte-text dark:text-mocha-text">
-      <section>
-        <h1 className="text-4xl font-bold">Contributors</h1>
-        <p>iloveportfolio is a collaboration project made by a small developer community ranging from veterans to newbies.</p>
+    <main className="flex h-full lg:w-[1200px] flex-col items-start text-latte-text dark:text-mocha-text p-10">
+      <section className="mt-20 lg:mt-0 flex flex-col gap-2">
+        <h1 className=" text-2xl md:text-3xl lg:text-5xl font-bold">Contributors</h1>
+        <p className="text-sm lg:text-lg">iloveportfolio is a collaboration project made by a small developer community ranging from veterans to newbies.</p>
       </section>
 
-      <section className="mt-14">
-        <section>
-          <h2 className="text-2xl font-bold">Back-end</h2>
-          <p>Contributors that develop back-end systems such as A.I. systems and Authentication systems.</p>
+      <section className="mt-8 lg:mt-14">
+        <section className="flex flex-col gap-2">
+          <h2 className="text-lg md:text-xl lg:text-3xl font-bold">Back-end</h2>
+          <p className="text-sm lg:text-lg">Contributors that develop back-end systems such as A.I. systems and Authentication systems.</p>
         </section>
-        <section className="flex w-[1200px] flex-row flex-wrap gap-5">
+        <section className="flex lg:w-[1200px] flex-row flex-wrap gap-5">
           {backend.map((item, index) => (
             <UserCard
               key={index}
@@ -57,12 +57,12 @@ export default function ContributorsPage() {
         </section>
       </section>
 
-      <section className="mt-12">
-        <section>
-          <h2 className="text-2xl font-bold">Front-end</h2>
-          <p>Contributors that develop aninteractive GUI for the users.</p>
+      <section className="mt-8 lg:mt-12">
+        <section className="flex flex-col gap-2">
+          <h2 className="text-lg md:text-xl lg:text-3xl font-bold">Front-end</h2>
+          <p className="text-sm lg:text-lg">Contributors that develop an interactive GUI for the users.</p>
         </section>
-        <section className="flex w-[1200px] flex-row flex-wrap gap-5">
+        <section className="flex lg:w-[1200px] flex-row flex-wrap gap-5">
           {frontend.map((item, index) => (
             <UserCard
               key={index}

e87c9f9b79bb37856e1037758f644e20807198e7
Yncy0 <paalayancy@gmail.com>
2025-06-26 20:48:35 +0800

    finished: contributor page

--- src/app/contributors/page.tsx
+++ src/app/contributors/page.tsx
@@ -5,13 +5,22 @@ export default function ContributorsPage() {
     imageUrl: string;
     link: string;
     username: string;
+    role?: string;
   };
 
   const backend: Contributors[] = [
     {
+      link: "https://github.com/Frostzeichen",
+      imageUrl: "/contributors/Frostzeichen.jpeg",
+      username: "Frostzeichen",
+      role: "AI/ML Developer",
+    },
+
+    {
       link: "https://github.com/xhide341",
       imageUrl: "/contributors/hide.jpeg",
       username: "hide",
+      role: "Back-end Developer",
     },
   ];
 
@@ -19,39 +28,51 @@ export default function ContributorsPage() {
     {
       link: "https://github.com/justinecua",
       imageUrl: "/contributors/cua.jpeg",
-      username: "Justine Cua",
+      username: "justinecua",
+      role: "Fronte-end Developer",
     },
     {
       link: "https://github.com/Yncy0",
       imageUrl: "/contributors/yncy0.png",
       username: "Yncy0",
+      role: "Fronte-end Developer",
     },
     {
       link: "https://github.com/xhide341",
       imageUrl: "/contributors/hide.jpeg",
-      username: "hide",
+      username: "xhide341",
+      role: "Fronte-end Developer",
     },
   ];
 
   return (
-    <main className="flex h-full lg:w-[1200px] flex-col items-start text-latte-text dark:text-mocha-text p-10">
-      <section className="mt-20 lg:mt-0 flex flex-col gap-2">
-        <h1 className=" text-2xl md:text-3xl lg:text-5xl font-bold">Contributors</h1>
-        <p className="text-sm lg:text-lg">iloveportfolio is a collaboration project made by a small developer community ranging from veterans to newbies.</p>
+    <main className="text-latte-text dark:text-mocha-text flex h-full flex-col items-start p-10 lg:w-[1200px]">
+      <section className="mt-20 flex flex-col gap-2 lg:mt-0">
+        <h1 className="text-2xl font-bold md:text-3xl lg:text-4xl">
+          Contributors
+        </h1>
+        <p className="text-sm lg:text-lg">
+          iloveportfolio is a collaboration project made by a small developer
+          community ranging from veterans to newbies.
+        </p>
       </section>
 
       <section className="mt-8 lg:mt-14">
         <section className="flex flex-col gap-2">
-          <h2 className="text-lg md:text-xl lg:text-3xl font-bold">Back-end</h2>
-          <p className="text-sm lg:text-lg">Contributors that develop back-end systems such as A.I. systems and Authentication systems.</p>
+          <h2 className="text-lg font-bold md:text-xl lg:text-3xl">Back-end</h2>
+          <p className="text-sm lg:text-lg">
+            Contributors that develop back-end systems such as A.I. systems and
+            Authentication systems.
+          </p>
         </section>
-        <section className="flex lg:w-[1200px] flex-row flex-wrap gap-5">
+        <section className="flex flex-row flex-wrap gap-5 lg:w-[1200px]">
           {backend.map((item, index) => (
             <UserCard
               key={index}
               imageUrl={item.imageUrl}
               link={item.link}
               username={item.username}
+              role={item.role}
             />
           ))}
         </section>
@@ -59,10 +80,14 @@ export default function ContributorsPage() {
 
       <section className="mt-8 lg:mt-12">
         <section className="flex flex-col gap-2">
-          <h2 className="text-lg md:text-xl lg:text-3xl font-bold">Front-end</h2>
-          <p className="text-sm lg:text-lg">Contributors that develop an interactive GUI for the users.</p>
+          <h2 className="text-lg font-bold md:text-xl lg:text-3xl">
+            Front-end
+          </h2>
+          <p className="text-sm lg:text-lg">
+            Contributors that develop an interactive GUI for the users.
+          </p>
         </section>
-        <section className="flex lg:w-[1200px] flex-row flex-wrap gap-5">
+        <section className="flex flex-row flex-wrap gap-5 lg:w-[1200px]">
           {frontend.map((item, index) => (
             <UserCard
               key={index}
--- src/components/contributors/card/card.tsx
+++ src/components/contributors/card/card.tsx
@@ -7,15 +7,24 @@ type Props = {
   imageUrl: string;
   link: string;
   username: string;
+  role?: string;
 };
 
 export default function UserCard(props: Props) {
   return (
-    <Card className="shadow-none border-none p-7 flex-1 bg-latte-crust dark:bg-mocha-crust text-latte-text dark:text-mocha-text">
+    <Card className="bg-latte-crust dark:bg-mocha-crust text-latte-text dark:text-mocha-text flex-1 lg:basis-[20%] border-none p-7 shadow-none">
       <CardContent>
         <Link href={props.link} className="flex flex-col items-center gap-5">
-          <Image src={props.imageUrl} alt={props.imageUrl} width={100} height={100} className="rounded-[50%]" />
-          <p>{props.username}</p>
+          <Image
+            src={props.imageUrl}
+            alt={props.imageUrl}
+            width={100}
+            height={100}
+            className="rounded-[50%]"
+          />
+          <p className="text-center">
+            {props.username} <br /> <span>{props.role}</span>
+          </p>
         </Link>
       </CardContent>
     </Card>
